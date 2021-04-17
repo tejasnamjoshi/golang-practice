09cf6d1130336bd54776aabf7cc1bf1bfc7a4db5
Tejas <tejasnamjoshi9@gmail.com>
2021-04-17 12:32:05 +0530

    Added auto caching.

--- Gophercizes/HackerNews/main.go
+++ Gophercizes/HackerNews/main.go
@@ -15,6 +15,12 @@ import (
 	"time"
 )
 
+// var (
+// 	cache      []item
+// 	cacheExp   time.Time
+// 	cacheMutex sync.Mutex
+// )
+
 func main() {
 	var port, numStories int
 	flag.IntVar(&port, "port", 3000, "Port number to start the application on.")
@@ -27,9 +33,36 @@ func main() {
 }
 
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+
+	sc := storyCache{
+		numStories: numStories,
+		duration:   6 * time.Second,
+	}
+
+	go func() {
+		ticket := time.NewTicker(3 * time.Second)
+		for {
+			// wait till ticker sends a tick
+			<-ticket.C
+			temp := storyCache{
+				numStories: numStories,
+				duration:   6 * time.Second,
+			}
+
+			temp.stories()
+			var cache []item
+			cache = temp.cache
+
+			sc.mutex.Lock()
+			sc.cache = cache
+			sc.exp = temp.exp
+			sc.mutex.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		stories, err := getCachedStories(numStories)
+		stories, err := sc.stories()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -42,27 +75,48 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	})
 }
 
-var (
+type storyCache struct {
+	numStories int
 	cache      []item
-	cacheExp   time.Time
-	cacheMutex sync.Mutex
-)
+	exp        time.Time
+	duration   time.Duration
+	mutex      sync.Mutex
+}
 
-func getCachedStories(numStories int) ([]item, error) {
-	cacheMutex.Lock()
-	defer cacheMutex.Unlock()
-	if time.Now().Sub(cacheExp) < 0 {
-		return cache, nil
+// Pointer because we want to maintain the same cache in the same ref.
+func (sc *storyCache) stories() ([]item, error) {
+
+	sc.mutex.Lock()
+	defer sc.mutex.Unlock()
+	if time.Now().Sub(sc.exp) < 0 {
+		return sc.cache, nil
 	}
-	stories, err := getTopStories(numStories)
+
+	stories, err := getTopStories(sc.numStories)
+	sc.exp = time.Now().Add(sc.duration)
 	if err != nil {
 		return nil, err
 	}
-	cache = stories
-	cacheExp = time.Now().Add(5 * time.Minute)
+	sc.cache = stories
+	return sc.cache, nil
 
-	return cache, nil
 }
+
+// func getCachedStories(numStories int) ([]item, error) {
+// 	cacheMutex.Lock()
+// 	defer cacheMutex.Unlock()
+// 	if time.Now().Sub(cacheExp) < 0 {
+// 		return cache, nil
+// 	}
+// 	stories, err := getTopStories(numStories)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	cache = stories
+// 	cacheExp = time.Now().Add(5 * time.Minute)
+
+// 	return cache, nil
+// }
 
 func getTopStories(numStories int) ([]item, error) {
 	var c hn.Client
